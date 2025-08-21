33d2cfb1ccb6ade43f54e8a77f982a552eadd660
bruno.calazans <bruno.calazans@presidencia.gov.br>
2025-08-21 17:30:29 -0300

    Implementar teste para método de divisão

---
+++ tests/test_divisao.py
@@ -0,0 +1,22 @@
+import pytest
+from Calculadora import Calculadora
+
+# Abrevia o nome da classe para simplificar
+@pytest.fixture
+def calc():
+    return Calculadora()
+
+@pytest.mark.parametrize("numerador, denominador, resultado_esperado", [
+    (1, 2, 0.5),
+    (500, 5, 10),
+    (80, 4, 20),
+    (44, 44, 1),
+    (77, 66, 11),
+])
+
+def test_divisao(calc, numerador, denominador, resultado_esperado):
+    assert calc.divisao(numerador, denominador) == resultado_esperado
+
+def test_divisao_por_zero(calc):
+    with pytest.raises(ZeroDivisionError):
+        calc.divisao(5, 0)
